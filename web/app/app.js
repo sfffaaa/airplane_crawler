12da7d6e167c0a63ac6576a188c09cd26f17cee2
Jay Pan <sfffaaa@gmail.com>
2017-02-24 22:39:44 +0800

    Fix activate tab
    But still have issue because the previous page doesn't activate the correct page.

--- web/app/app.js
+++ web/app/app.js
@@ -16,3 +16,18 @@ config(['$routeProvider', function($routeProvider) {
         redirectTo: '/peach'
     });
 }]);
+
+$(function() {
+    var url_parts = location.href.split('/');
+    var interested_idx = url_parts.length-2;
+    var last_segment = url_parts.slice(interested_idx, interested_idx + 2).join('/');
+    $('a.aircompany-tab[href="' + last_segment + '"]').parents('li').addClass('active');
+});
+
+$('a.aircompany-tab').on('click', function (e) {
+    $('li a.aircompany-tab').each(function() {
+        $(this).parent().removeClass("active")
+    });
+    var target = $(e.target).parent().addClass("active");
+});
+
